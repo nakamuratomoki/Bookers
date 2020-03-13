b0bd53d5ce31b23f192b1a68dcd42737e374b6ba
中村　智貴 <nakamuratomoki@nakamurasatoshitakanoMacBook-ea.local>
2020-03-13 20:37:19 +0900

    Initial commit 1

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Book.create(title: 'トラブル知らずのシステム設計', body: 'システム設計の要点を図解で説明してくれます')
+Book.create(title: 'たのしいRuby', body: 'Rubyの入門におすすめです')
+Book.create(title: '入門Git', body: 'gitについての基本操作から内部構造まで解説してくれます')
+Book.create(title: 'アルゴリズム図鑑', body: 'アルゴリズムについてカラーイラストでしっかり解説してくれます')
